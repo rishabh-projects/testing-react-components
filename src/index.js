30aca4e0a4086a451036cd56b3889b6f90ee15d2
Rishabh Mewar <rmewar.dev@gmail.com>
2021-05-16 22:22:37 +0530

    Building Mars Rover Images UI Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import ConnectedRoverSearch, {reducer as storeReducer} from './pages/ConnectedRoverSearch';
 import * as serviceWorker from './serviceWorker';
+import promise from 'redux-promise-middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// NOTE: this section has been updated to work with browsers that don't have the devTools extension
+const finalCreateStore = compose(
+    applyMiddleware(promise),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )(createStore);
+const store = finalCreateStore(storeReducer);
+
+ReactDOM.render(<Provider store={store}><ConnectedRoverSearch /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
